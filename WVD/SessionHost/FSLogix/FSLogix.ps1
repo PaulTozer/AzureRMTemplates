57b80baf0d19a88a2a2a2c7e86f86d2dd40d6eef
Paul Tozer <45483044+PaulTozer@users.noreply.github.com>
2020-09-15 12:58:34 +0100

    Create FSLogix.ps1

---
+++ WVD/SessionHost/FSLogix/FSLogix.ps1
@@ -0,0 +1,116 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ExecutableName = "FSLogixApp.zip"
+
+)
+
+#####################################
+
+##########
+# Helper #
+##########
+#region Functions
+function LogInfo($message) {
+    Log "Info" $message
+}
+
+function LogError($message) {
+    Log "Error" $message
+}
+
+function LogSkip($message) {
+    Log "Skip" $message
+}
+function LogWarning($message) {
+    Log "Warning" $message
+}
+
+function Log {
+
+    <#
+    .SYNOPSIS
+   Creates a log file and stores logs based on categories with tab seperation
+    .PARAMETER category
+    Category to put into the trace
+    .PARAMETER message
+    Message to be loged
+    .EXAMPLE
+    Log 'Info' 'Message'
+    #>
+
+    Param (
+        $category = 'Info',
+        [Parameter(Mandatory = $true)]
+        $message
+    )
+
+    $date = get-date
+    $content = "[$date]`t$category`t`t$message`n"
+    Write-Verbose "$content" -verbose
+
+    if (! $script:Log) {
+        $File = Join-Path $env:TEMP "log.log"
+        Write-Error "Log file not found, create new $File"
+        $script:Log = $File
+    }
+    else {
+        $File = $script:Log
+    }
+    Add-Content $File $content -ErrorAction Stop
+}
+
+function Set-Logger {
+    <#
+    .SYNOPSIS
+    Sets default log file and stores in a script accessible variable $script:Log
+    Log File name "executionCustomScriptExtension_$date.log"
+    .PARAMETER Path
+    Path to the log file
+    .EXAMPLE
+    Set-Logger
+    Create a logger in
+    #>
+
+    Param (
+        [Parameter(Mandatory = $true)]
+        $Path
+    )
+
+    # Create central log file with given date
+
+    $date = Get-Date -UFormat "%Y-%m-%d %H-%M-%S"
+
+    $scriptName = (Get-Item $PSCommandPath ).Basename
+    $scriptName = $scriptName -replace "-", ""
+
+    Set-Variable logFile -Scope Script
+    $script:logFile = "executionCustomScriptExtension_" + $scriptName + "_" + $date + ".log"
+
+    if ((Test-Path $path ) -eq $false) {
+        $null = New-Item -Path $path -type directory
+    }
+
+    $script:Log = Join-Path $path $logfile
+
+    Add-Content $script:Log "Date`t`t`tCategory`t`tDetails"
+}
+#endregion
+
+Set-Logger "C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\executionLog\FSLogix" # inside "executionCustomScriptExtension_$scriptName_$date.log"
+
+$Uri = "https://aks.ms/FSLogixDownload"
+Invoke-WebRequest -Uri $Uri -OutFile "$($PSScriptRoot)\$ExecutableName"
+$FSLogixArchivePath = Join-Path $PSScriptRoot "FSLogixApp.zip"
+Expand-Archive -Path $FSLogixArchivePath -DestinationPath $PSScriptRoot
+
+$ExecutableName = "x64\Release\FSLogixAppsSetup.exe"
+$FSLogixExePath = Join-Path $PSScriptRoot $ExecutableName
+
+$Switches = "/passive /norestart"
+
+$Installer = Start-Process -FilePath $FSLogixExePath -ArgumentList $Switches -Wait -PassThru
+
+LogInfo("The exit code is $($Installer.ExitCode)")
