3e1cca900a8581f630207585c11a8c39538b8bb1
Paul Tozer <45483044+PaulTozer@users.noreply.github.com>
2023-10-27 14:42:32 +0100

    Add files via upload

--- WVD/SessionHostImageCreator/M365Apps/M365AppsBasic.ps1
+++ WVD/SessionHostImageCreator/M365Apps/M365AppsBasic.ps1
@@ -1,10 +1,12 @@
+$FolderName = "C:\Packages\M365"
+New-Item $FolderName -ItemType Directory -Force
 $ExecutableName = "OfficeDeploy.zip"
+$path = "$($FolderName)\$ExecutableName"
 $Uri = "https://raw.githubusercontent.com/PaulTozer/AzureRMTemplates/master/WVD/SessionHostImageCreator/M365Apps/OfficeDeploy.zip"
-Invoke-WebRequest -Uri $Uri -OutFile "$($PSScriptRoot)\$ExecutableName"
-$M365ArchivePath = Join-Path $PSScriptRoot "OfficeDeploy.zip"
-Expand-Archive -Path $M365ArchivePath -DestinationPath $PSScriptRoot
-$ExecutableName = "OfficeDeploy\setup.exe"
+Invoke-WebRequest -Uri $Uri -OutFile $path
+Expand-Archive -Path $path -DestinationPath $foldername -Force
+$InstallExec = "OfficeDeploy\setup.exe"
 $Switches = "/configure .\OfficeDeploy\Configuration.xml"
-$OfficeExePath = Join-Path $PSScriptRoot $ExecutableName
+$OfficeExePath = Join-Path $path $InstallExec
 $Installer = Start-Process -FilePath $OfficeExePath -ArgumentList $Switches -Wait -PassThru
 
